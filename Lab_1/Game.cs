872b901516594f16de6959929a3f6e2c5c8f76f6
“[Rajaram <rajaramkarki89@gmail.com>
2023-03-17 09:25:27 +0545

    Made the Border with Two Inner Triangles

--- Lab_1/Game.cs
+++ Lab_1/Game.cs
@@ -8,6 +8,7 @@ using OpenTK;
 using OpenTK.Mathematics;
 using OpenTK.Windowing.Common;
 using OpenTK.Windowing.Desktop;
+using System.Xml.Linq;
 
 namespace Lab_1
 {
@@ -35,25 +36,25 @@ namespace Lab_1
 
             float[] vertices = new float[]
             {
-                // Triangle 1 - top
-                -1.0f, 0.4f, 0.0f, // top
-                -1.0f, -0.5f, 0.0f, // bottom left
-                0.0f, -0.5f, 0.0f, // bottom right
+                // Triangle 1 - bottom
+                -0.5f, 0.35f, 0.0f, // top
+                -0.5f, -0.5f, 0.0f, // bottom left
+                0.5f, -0.5f, 0.0f, // bottom right
 
-                // Triangle 2 - bottom
-                -1.0f, 0.5f, 0.0f, // bottom
-                -1.0f, 0.0f, 0.0f, // top right
-                0.0f, 0.0f, 0.0f, // top left
+                // Triangle 2 - top
+                -0.5f, 0.6f, 0.0f, // top
+                -0.5f, 0.0f, 0.0f, // bottom right
+                0.5f, 0.0f, 0.0f, // bottom left
 
-                //vertices for 1st border
-                -1.0f, 0.5f, 0.0f,
+                //Inner Triangle Bot
+                -0.47f, -0.47f, 0.0f,   //bottom left
+                0.42f, -0.47f, 0.0f,    //bottom right
+                -0.47f, 0.28f, 0.0f,       //top
 
-            };
-
-            int[] indices = new int[]
-            {
-                0, 1, 2,
-                3, 4, 5,
+                //Inner triangle top
+                -0.47f, 0.03f, 0.0f,   //bottom left
+                -0.47f, 0.54f, 0.0f,     //top
+                0.38f, 0.03f, 0.0f       //bottom right
 
             };
 
@@ -78,64 +79,42 @@ namespace Lab_1
                  layout (location=0) in vec3 aPosition;
                  
                  void main(){
-                 vec3 translatedPosition = vec3(aPosition.x + 0.5f, aPosition.y, aPosition.z);
-                 gl_Position = vec4(translatedPosition, 1.0f);
+                 
+                    gl_Position = vec4(aPosition, 1.0f);
 
                  }";
 
             string pixelShaderCode =
                 @"
                  #version 330 core
-
-                 out vec4 pixelColor;
+                 out vec4 fragColor;
+                 uniform vec4 vColor;
 
                  void main(){
-
-                    pixelColor = vec4(0.8627f, 0.0784f, 0.2353f, 1.0f);
+                    fragColor = vColor;
                  }
                  ";
 
-
-
-    //        string vertexShaderCode =
-    //@"
-    //             #version 330 core
-
-    //             layout (location=0) in vec3 aPosition;
-    //             out int vertexIndex;
-     
-    //             void main(){
-    //                 vertexIndex = gl_VertexID;
-    //                 vec3 translatedPosition = vec3(aPosition.x + 0.5f, aPosition.y, aPosition.z);
-    //                 gl_Position = vec4(translatedPosition, 1.0f);
-    //             }";
-
-    //        string pixelShaderCode =
-    //            @"
-    //             #version 330 core
-     
-    //             in int vertexIndex;
-    //             out vec4 pixelColor;
-
-    //             void main(){
-    //                if(vertexIndex >= 6)
-    //                {
-    //                    pixelColor = vec4(0.0f, 0.0f, 1.0f, 1.0f); // Blue color for line
-    //                }
-    //                else
-    //                {
-    //                    pixelColor = vec4(0.8627f, 0.0784f, 0.2353f, 1.0f); // Red color for triangles
-    //                }
-    //             }";
 
             int vertexShaderObject = GL.CreateShader(ShaderType.VertexShader);
             GL.ShaderSource(vertexbufferObject, vertexShaderCode);
             GL.CompileShader(vertexShaderObject);
 
+            string vertexShaderInfo = GL.GetShaderInfoLog(vertexShaderObject);
+            if(vertexShaderInfo != String.Empty)
+            {
+                Console.WriteLine(vertexShaderInfo);
+            }
 
             int pixelShaderObject = GL.CreateShader(ShaderType.FragmentShader);
             GL.ShaderSource(pixelShaderObject, pixelShaderCode);
             GL.CompileShader(pixelShaderObject);
+
+            string pixelShaderInfo = GL.GetShaderInfoLog(pixelShaderObject);
+            if (pixelShaderInfo != String.Empty)
+            {
+                Console.WriteLine(pixelShaderInfo);
+            }
 
             this.shaderProgramObject = GL.CreateProgram();
 
@@ -159,7 +138,7 @@ namespace Lab_1
             GL.DeleteBuffer(this.vertexbufferObject);//check this in case
 
             GL.UseProgram(0);//check this too
-            GL.DeleteProgram(this.shaderProgramObject);//Problem identified
+            GL.DeleteProgram(this.shaderProgramObject);
 
 
             base.OnUnload();
@@ -170,7 +149,19 @@ namespace Lab_1
             base.OnResize(e);
         }
 
+        public float[] TransformVertex(float[] arr, float x, float y)
+        {
+            float[] final_vertices = new float[arr.Length];
 
+            for (int i =0; i<arr.Length; i= i+3)
+            {
+                final_vertices[i] = arr[i] + x;
+                final_vertices[i + 1] = arr[i + 1] + y;
+                final_vertices[i + 2] = 0;
+            }
+
+            return final_vertices;
+        }
 
         protected override void OnRenderFrame(FrameEventArgs args)
         {
@@ -178,12 +169,43 @@ namespace Lab_1
             GL.Clear(ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit);
 
             GL.UseProgram(this.shaderProgramObject);
+            int uniformLocations = GL.GetUniformLocation(this.shaderProgramObject, "vColor");
+            GL.Uniform4(uniformLocations, 0.078f, 0.114f, 0.420f, 1.0f);
+
+
+
+            ////////checking
+            //float[] array = {-1.0f, 0.4f, 0.0f,
+            //                 -1.0f, -0.5f, 0.0f,
+            //                 0.0f, -0.5f, 0.0f};
+
+
+            ////checking for the vertices transform
+            //float[] final_vertices = TransformVertex(array, 1f, 1f);
+            //foreach (int i in final_vertices)
+            //{
+            //    Console.WriteLine("{0}", i);
+            //}
+            /////checking finish
+
 
             GL.BindVertexArray(this.vertexArrayObject);
 
+
+            ///for outer two triangles that are blue
             GL.DrawArrays(PrimitiveType.Triangles, 0, 3); // left triangle
             GL.DrawArrays(PrimitiveType.Triangles, 3, 3); // right triangle
-            //GL.DrawArrays(PrimitiveType.LineLoop, 2, 6);
+
+            ///for inner two triangles that are crimson
+            GL.Uniform4(uniformLocations, 0.8627f, 0.0784f, 0.2353f, 1.0f);     //crimson color
+            GL.DrawArrays(PrimitiveType.Triangles, 6, 3);
+            GL.DrawArrays(PrimitiveType.Triangles, 9, 3);
+
+
+
+            ///for the sun and moon
+            GL.Uniform4(uniformLocations, 1.0f, 1.0f, 0.0f, 1.0f); // yellow
+
 
             this.Context.SwapBuffers();
             base.OnRenderFrame(args);
